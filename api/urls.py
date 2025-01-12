5442787facac211d3ee5b2aa1cb839e23da68239
conrad <conradmax5@gmail.com>
2025-01-12 16:29:58 +0300

    database and product CRUD operations working

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('api/',views.index, name='index'),
+    path('api/products',views.products_list, name='products_list'),
+    path('api/add-product',views.add_product, name='add_product'),
+
+    path('api/edit-product/<int:id>',views.edit_product, name='edit_product'),
+    path('api/delete-product/<int:id>',views.delete_product, name='delete_product'),
 ]
